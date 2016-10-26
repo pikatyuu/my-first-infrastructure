e095437d4872a519ca248fd7a49f75322ac2e081
杉山 <yuu_16n1200018@nnn.ed.jp>
2016-10-26 17:27:23 +0900

    Add syslog file for logrotate

--- init/recipe.rb
+++ init/recipe.rb
@@ -10,8 +10,18 @@ execute "install logrotate" do
   command "aptitude install logrotate"
 end
 
+execute "remove all file on /etc/logrotate.d except apt file" do
+  command "rm `ls /etc/logrotate.d -I apt`"
+end
+
 template "/etc/logrotate.d" do
   source "./docker"
+  owner "root"
+  group "root"
+end
+
+template "/etc/logrotate.d" do
+  source "./rsyslog"
   owner "root"
   group "root"
 end
